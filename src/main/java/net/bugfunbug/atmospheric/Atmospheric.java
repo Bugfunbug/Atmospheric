ab198176f2883c28b7416b9cfb67deedbb0ec429
Bugfunbug <ethanlaib1@gmail.com>
2025-12-29 20:34:13 -0500

    Initial commit.

--- src/main/java/net/bugfunbug/atmospheric/Atmospheric.java
+++ src/main/java/net/bugfunbug/atmospheric/Atmospheric.java
@@ -8,17 +8,16 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Atmospheric.MOD_ID)
+@Mod(Atmospheric.MODID)
 public class Atmospheric {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "atmospheric";
+    public static final String MODID = "atmospheric";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -49,7 +48,7 @@ public class Atmospheric {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
